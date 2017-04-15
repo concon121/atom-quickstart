0843b957dea881d24e29a88abc0f9a4510d6158a
Connor Bray <connor.bray@icloud.com>
2017-04-15 20:47:01 +0100

    Updated atom-quickstart configuration: /home/connor/workspace/atom-quickstart/ / CLOSE_WRITE,CLOSE .install.sh.swx

--- install.sh
+++ install.sh
@@ -1,36 +1,51 @@
 #!/bin/bash
+set -x
 
-currentDir=`pwd`
+running=`ps -aux | grep "/bin/bash /usr/bin/atom-quickstart" | wc -l`
 
-installed=`which atom`
-if [ -z "${installed}" ]
+if [ "${running}" -lt 4 ]
 then
-  sudo add-apt-repository ppa:webupd8team/atom
-  sudo apt-get -y update
-  sudo apt-get -y install atom
+  echo "Starting atom-quickstart"
+  currentDir=`pwd`
+
+  installed=`which atom`
+  if [ -z "${installed}" ]
+  then
+    echo "Installing atom.io"
+    sudo add-apt-repository ppa:webupd8team/atom
+    sudo apt-get -y update
+    sudo apt-get -y install atom
+  else 
+    echo "Atom.io is already installed!"
+  fi
+
+  echo "Installing atom.io plugins"
+  plugins=(`ls plugins`)
+  for plugin in "${plugins[@]}"
+  do
+    ./plugins/${plugin}
+  done
+
+  echo "Ensuring atom.io plugins are up to date"
+  sudo apm upgrade -c false
+
+  echo "Moving atom.io configuration"
+  ln -sf "${currentDir}/config.cson" $HOME/.atom/config.cson
+  ln -sf "${currentDir}/init.coffee" $HOME/.atom/init.coffee
+  ln -sf "${currentDir}/keymap.cson" $HOME/.atom/keymap.cson
+  ln -sf "${currentDir}/snippets.cson" $HOME/.atom/snippets.cson
+  ln -sf "${currentDir}/styles.less" $HOME/.atom/styles.less
+
+  echo "Watching atom.io configuration for changes"
+  sudo apt-get -y install inotify-tools
+  inotifywait -q -m -r -e close_write "${currentDir}" |
+  while read -r filename event; do
+    sleep 60
+    git pull origin master
+    git add .
+    git commit -m "Updated atom-quickstart configuration: ${filename} / ${event}"
+    git push origin master
+  done
+else
+  echo "atom-quickstart is already running"
 fi
-
-plugins=(`ls plugins`)
-for plugin in "${plugins[@]}"
-do
-  ./plugins/${plugin}
-done
-
-sudo apm upgrade
-
-ln -sf "${currentDir}/config.cson" $HOME/.atom/config.cson
-ln -sf "${currentDir}/init.coffee" $HOME/.atom/init.coffee
-ln -sf "${currentDir}/keymap.cson" $HOME/.atom/keymap.cson
-ln -sf "${currentDir}/snippets.cson" $HOME/.atom/snippets.cson
-ln -sf "${currentDir}/styles.less" $HOME/.atom/styles.less
-
-sudo apt-get -y install inotify-tools
-
-inotifywait -q -m -r -e close_write "${currentDir}" |
-while read -r filename event; do
-  sleep 60
-  git pull origin master
-  git add .
-  git commit -m "Updated atom-quickstart configuration: ${filename} / ${event}"
-  git push origin master
-done
