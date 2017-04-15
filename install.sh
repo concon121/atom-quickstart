ff6e2d07eeb20c1d33d3b557fcbc74735eee3afb
Connor Bray <connor.bray@icloud.com>
2017-04-15 19:31:11 +0100

    Updated atom-quickstart configuration: /home/connor/workspace/atom-quickstart/ / CLOSE_WRITE,CLOSE .README.md.swx

--- install.sh
+++ install.sh
@@ -17,13 +17,17 @@ do
 done
 
 ln -sf "${currentDir}/config.cson" $HOME/.atom/config.cson
+ln -sf "${currentDir}/init.coffee" $HOME/.atom/init.coffee
+ln -sf "${currentDir}/keymap.cson" $HOME/.atom/keymap.cson
+ln -sf "${currentDir}/snippets.cson" $HOME/.atom/snippets.cson
+ln -sf "${currentDir}/styles.less" $HOME/.atom/styles.less
 
 sudo apt-get -y install inotify-tools
 
-inotifywait -q -m -e close_write "${currentDir}/config.cson" |
+inotifywait -q -m -r -e close_write "${currentDir}" |
 while read -r filename event; do
   git pull origin master
   git add .
-  git commit -m "Updated atom.io configuration"
+  git commit -m "Updated atom-quickstart configuration: ${filename} / ${event}"
   git push origin master
 done
